2980a1c9515dcee52f1298b02c9aead152cddadd
Unknown <hartigan@purdue.edu>
2019-03-31 23:25:34 -0400

    Finished up preliminary script
    Potential TODOs: increase speed of data gathering

--- data-aggr.py
+++ data-aggr.py
@@ -19,22 +19,26 @@ class SerialPort():
         print("\nInitializing {} on port {}...".format(name, port))
         self.name = name
         self.port = port    # serial connection port
-        self.blankDict = {"altitude": None,
+        self.blankDict = {"Pressure": [],   # Initialize dictionary structure
         "GPS": {
-            "longitude": None,
-            "latitude": None
+            "longitude": [],
+            "latitude": []
             },
         "Gyroscope": {
-            "x": None,
-            "y": None,
-            "z": None
+            "x": [],
+            "y": [],
+            "z": []
             },
-        "Magnetometer": None,
-        "Temperature": None,
+        "Magnetometer": {
+            "x": [],
+            "y": [],
+            "z": []
+            },
+        "Temperature": [],
         "Accelerometer": {
-            "x": None,
-            "y": None,
-            "z": None
+            "x": [],
+            "y": [],
+            "z": []
             }
         }
         self.ser = serial.Serial(self.port, 115200)   # -, baud rate (from Arduino)
@@ -49,20 +53,31 @@ class SerialPort():
         Args:
             file: .json file to write on
         """
-        ln = self.ser.readline()
+        ln = self.ser.readline().rstrip()
         if ln.split(':')[0] == "ERROR":
-            pass
+            pass    # Don't write if given an error message
         else:
-            '''
-            lst = ln.split(',')
-            # TODO: Work out printing
-            '''
-            print(ln)
+            # Break string into array
+            lst = ln.split(', ')
+            # Assigning each value in given list to dict entry
+            self.currentDict["Pressure"].append(lst[0])
+            self.currentDict["GPS"]["longitude"].append(lst[1])
+            self.currentDict["GPS"]["latitude"].append(lst[2])
+            self.currentDict["Gyroscope"]["x"].append(lst[3])
+            self.currentDict["Gyroscope"]["y"].append(lst[4])
+            self.currentDict["Gyroscope"]["z"].append(lst[5])
+            self.currentDict["Magnetometer"]["x"].append(lst[6])
+            self.currentDict["Magnetometer"]["y"].append(lst[7])
+            self.currentDict["Magnetometer"]["z"].append(lst[8])
+            self.currentDict["Temperature"].append(lst[9])
+            self.currentDict["Accelerometer"]["x"].append(lst[10])
+            self.currentDict["Accelerometer"]["y"].append(lst[11])
+            self.currentDict["Accelerometer"]["z"].append(lst[12])
 
     def JSONpass(self, fname):
         """
-        Creates a .json file every second and populates it with data from
-        serial connection
+        Creates a .json file and populates it with one second's worth of data
+        from serial connection
 
         Args:
             fname: current date name for json file
@@ -78,8 +93,7 @@ class SerialPort():
 
 if __name__ == "__main__":
     # Create new serial port for sensor arduino with name and USB port path
-    # TODO: Find actual path on Pi
-    ino = SerialPort("SensorModule", "/dev/tty")
+    ino = SerialPort("SensorModule", "/dev/ttyUSB0")
 
     while True: # Iterates infinitely, writing new .json files to ~/Avionics/send
         ino.JSONpass(str(int(time.time()*1000)) + ".json")   # ms since 1/1/1970
