d0801fc0ecd84998ee00d6442b8ff1103aaf7803
Mark Hartigan <mark@raspberrypi>
2019-10-13 15:02:20 -0400

    Added logging capabilities for the MPU9250 sensor
    Gyro, temp, acceleration, mag

--- balloon/src/data_aggr.py
+++ balloon/src/data_aggr.py
@@ -54,7 +54,8 @@ class Sensors():
         self.log.write('time (s),alt (m),long,lat,g_x (dps),g_y (dps),g_z (dps),m_x (mT),m_y (mT),m_z (mT),temp (C),a_x (mg),a_y (mg),a_z (mg)\n')
 
         self.imu = mpu9250()  # Create IMU Object from mpu9250.py
- 
+        self.start = time.time()
+        
         """
         try:
             self.radio = Module.get_instance(self)  # Initialize radio communication
@@ -68,29 +69,30 @@ class Sensors():
         Reads from Serial connection and writes to dict
         """
 
-        gyroX, gyroY, gyroZ = readGyro()
-        magX, magY, magZ = readMagnet()
-        gpsLat, gpsLong, gpsAlt = readGPS()
-        accelX, accelY, accelZ = readAccel()
-        temp = readTemperature()
-
+        gx, gy, gz = self.readGyro()
+        mx, my, mz = self.readMagnet()      # works (uncalibrated)
+        lat, lon, alt = self.readGPS()  # works (uncalibrated)
+        ax, ay, az = self.readAccel() # works (uncalibrated)
+        temp = self.readTemperature()             # works (uncalibrated)
+        t = time.time() - self.start
+        
         # Write to .log file
-        self.log.write(f'{0},{gpsAlt},{gpsLong},{gpsLat},{gyroX},{gyroY},{gyroZ},{magX},{magY},{magZ},{temp},{accelX},{accelY},{accelZ}\n')
+        self.log.write("{:.3f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f}\n".format(t,alt,lon,lat,gx,gy,gz,mx,my,mz,temp,ax,ay,az))
         
         # Assigning each value in given list to dict entry
-        self.json["Altitude"] = gpsAlt
-        self.json["GPS"]["longitude"] = gpsLong
-        self.json["GPS"]["latitude"] = gpsLat
-        self.json["Gyroscope"]["x"] = gyroX
-        self.json["Gyroscope"]["y"] = gyroY
-        self.json["Gyroscope"]["z"] = gyroZ
-        self.json["Magnetometer"]["x"] = magX
-        self.json["Magnetometer"]["y"] = magY
-        self.json["Magnetometer"]["z"] = magZ
+        self.json["Altitude"] = alt
+        self.json["GPS"]["longitude"] = lon
+        self.json["GPS"]["latitude"] = lat
+        self.json["Gyroscope"]["x"] = gx
+        self.json["Gyroscope"]["y"] = gy
+        self.json["Gyroscope"]["z"] = gz
+        self.json["Magnetometer"]["x"] = mx
+        self.json["Magnetometer"]["y"] = my
+        self.json["Magnetometer"]["z"] = mz
         self.json["Temperature"] = temp
-        self.json["Accelerometer"]["x"] = accelX
-        self.json["Accelerometer"]["y"] = accelY
-        self.json["Accelerometer"]["z"] = accelZ
+        self.json["Accelerometer"]["x"] = ax
+        self.json["Accelerometer"]["y"] = ay
+        self.json["Accelerometer"]["z"] = az
 
     def JSONpass(self, manager):
         """
@@ -136,28 +138,36 @@ class Sensors():
         """
         Reads acceleration from the MPU9250 chip
         """
-        return self.imu.accel()
+        return self.imu.accel
 
     def readGyro(self):
         """
         Reads gyroscopic data from the MPU9250 chip
         """
-        return self.imu.gyro()
+        return self.imu.gyro
 
     def readMagnet(self):
         """
         Read magnetometer data from the MPU9250 chip
         """
-        return self.imu.mag()
+        return self.imu.mag
 
     def readTemperature(self):
         """
         Reads temperature data from the MS5611 chip
         """
-        return self.imu.temp()
+        return self.imu.temp
 
     def readGPS(self):
         """
         Reads GPS data from the NEO7M chip
         """
-        return (longitude, latitude, altitude)
+        return (0, 0, 0)
+
+if __name__ == "__main__":
+    print("Running data_aggr.py ...\n")
+
+    sens = Sensors("MPU9250")
+
+    while True:
+        sens.writeDict()
