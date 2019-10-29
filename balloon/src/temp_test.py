5b5959689f7113161637052275cadf3add31dd88
Karina Abraham <kabraham@raspberrypi>
2019-10-28 20:22:14 -0400

    temp test progress

--- balloon/src/temp_test.py
+++ balloon/src/temp_test.py
@@ -1,11 +1,18 @@
-from data_aggr_old import SerialPort
-from time import sleep
+import time
+import sys, os
 
-if __name__ == "__main__":
-    sensors = SerialPort("Arduino", "/dev/ttyUSB0")
+from datetime import datetime
+from mpu9 import mpu9250
 
-    try:
-        while True:
-            sensors.writeDict()
-    except KeyboardInterrupt:
-        print('\nEnding experiment...\n')
+imu = mpu9250()
+temp = self.imu.temp()
+
+sTime = datetime.now()
+log = open('../logs/dataTemp.log', 'w+')    #Currently set to overwrite the current file for testing purposes. PLEASE CHANGE THIS TO a+ BEFORE YOU TEST!!
+log.write('time (s) temp (C)\n')
+
+for x in range(0,5):
+    print(f'{datetime.now().second - sTime.second} {temp}')
+    log.write(f'{datetime.now().second - sTime.second} {temp}\n')
+    time.sleep(1)
+
