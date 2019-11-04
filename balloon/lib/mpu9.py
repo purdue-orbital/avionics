997a5437a9c742649e3b3dc322fc18b2d02cb5f4
Mark Hartigan <hartigan@purdue.edu>
2019-11-04 13:47:48 -0500

    Changed data collection formatting to be friendlier

--- balloon/lib/mpu9.py
+++ balloon/lib/mpu9.py
@@ -1,12 +1,10 @@
 from time import sleep
 from i2c_device import I2CDevice
-import struct
 
 ################################
 # MPU9250
 ################################
 MPU9250_ADDRESS = 0x69
-AK8963_ADDRESS  = 0x0C
 DEVICE_ID       = 0x71
 WHO_AM_I        = 0x75
 PWR_MGMT_1      = 0x6B
@@ -30,73 +28,6 @@ GYRO_500DPS  = (0x01 << 3)
 GYRO_1000DPS = (0x02 << 3)
 GYRO_2000DPS = (0x03 << 3)
 # --- AK8963 ------------------
-MAGNET_DATA  = 0x03
-AK_DEVICE_ID = 0x48
-AK_WHO_AM_I  = 0x00
-AK8963_8HZ   = 0x02
-AK8963_100HZ = 0x06
-AK8963_14BIT = 0x00
-AK8963_16BIT = (0x01 << 4)
-AK8963_CNTL1 = 0x0A
-AK8963_CNTL2 = 0x0B
-AK8963_ASAX  = 0x10
-AK8963_ST1   = 0x02
-
-class AK8693(I2CDevice):
-    def __init__(self, name, ak_address=AK8693_ADDRESS):
-        """
-        Setup the Magnetometer
-
-        Needs to be initialized by the MPU9250 s.t. they are on the same network
-        """
-        super().__init__(self, ak_address, "AK8693")
-
-        if self.read(AK_WHO_AM_I) is not AK_DEVICE_ID:
-            raise Exception('AK8963: init failed to find device')
-        self.write(AK8963_CNTL1, (AK8963_16BIT | AK8963_8HZ))
-
-        # all 3 are set to 16b or 14b readings, we have take half, so one bit is
-        # removed 16 -> 15 or 13 -> 14
-        self.alsb = 2 / 2**15
-        self.glsb = 250 / 2**15
-        self.mlsb = 4800 / 2**15
-        
-    def read_xyz(self, address, register, lsb):
-        """
-        Reads x, y, and z axes at once and turns them into a tuple.
-        """
-        # data is MSB, LSB, MSB, LSB ...
-        data = self.bus.read_i2c_block_data(address, register, 6)
-
-        # data = []
-        # for i in range(6):
-        #       data.append(self.read8(address, register + i))
-
-        x = self.conv(data[0], data[1]) * lsb
-        y = self.conv(data[2], data[3]) * lsb
-        z = self.conv(data[4], data[5]) * lsb
-
-        # print('>> data', data)
-        # ans = self.convv.unpack(*data)
-        # ans = struct.unpack('<hhh', data)[0]
-        # print('func', x, y, z)
-        # print('struct', ans)
-
-        return (x, y, z)
-
-    def conv(self, msb, lsb):
-        """
-        http://stackoverflow.com/questions/26641664/twos-complement-of-hex-number-in-python
-        """
-        value = lsb | (msb << 8)
-        if value >= (1 << 15):
-            value -= (1 << 15)
-        # print(lsb, msb, value)
-        return value
-    
-    @property
-    def mag(self):
-        return self.read_xyz(AK8963_ADDRESS, MAGNET_DATA, self.mlsb)
 
         
 class MPU9250(I2CDevice):
@@ -140,10 +71,6 @@ class MPU9250(I2CDevice):
         # removed 16 -> 15 or 13 -> 14
         self.alsb = 2 / 2**15
         self.glsb = 250 / 2**15
-        self.mlsb = 4800 / 2**15
-
-        # i think i can do this???
-        # self.convv = struct.Struct('<hhh')
 
     def read16(self, address, register):
         data = self.bus.read_i2c_block_data(address, register, 2)
