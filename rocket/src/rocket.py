3c06628a40e68fc4ed8885231be419dc9bc4fb28
Automage <pranav92000@gmail.com>
2019-11-11 20:31:03 -0500

    Logging template finished

--- rocket/src/rocket.py
+++ rocket/src/rocket.py
@@ -5,14 +5,6 @@ from data_aggr import Sensors
 balloon_out_pin = 27
 balloon_in_pin = 22
 
-def balloon_disconnect(log):
-    """
-    Callback function when balloon_out_pin falls
-
-    Writes launch time to log file
-    """
-    ### TODO: Finish writing launch time + recieving time from ds32 ###
-    log.write()
 
 if __name__ == "__main__":
     # Set balloon_out_pin to high
@@ -21,16 +13,20 @@ if __name__ == "__main__":
     GPIO.setup(balloon_out_pin, GPIO.OUT)
     GPIO.output(balloon_out_pin, GPIO.HIGH)
 
-    # Set rising edge detection on balloon_in_pin
+    # Set falling edge detection on balloon_in_pin
 
     GPIO.setup(balloon_in_pin, GPIO.IN)
-    GPIO.add_event_detect(balloon_in_pin, GPIO.RISING, callback=balloon_disconnect(sens.log))
+    GPIO.wait_for_edge(balloon_in_pin, GPIO.FALLING)
+
+    # Log time of balloon disconnect
+
+    sens = Sensors("Rocket Computer") 
+    sens.log.write("balloon disconnect @ ")
 
     # Start data logging
+
     print("Running data_aggr.py ...\n")
     
-    sens = Sensors("Rocket Computer")
-
     while True:
         sens.read_all()
         sens.printd()
