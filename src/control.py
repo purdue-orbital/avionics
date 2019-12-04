e7ea29b984bc35dd61c57fd2e22e76787ae40ece
tremael Arrington <timewizard2500@gmail.com>
2019-12-04 18:32:52 -0500

    Added logging statements

--- src/control.py
+++ src/control.py
@@ -2,6 +2,7 @@ import sys, os
 import time
 import json, math
 import queue
+import logging
 from math import atan, pi
 import RPi.GPIO as GPIO
 from array import *
@@ -31,31 +32,38 @@ class Control:
 
         # GPIO SETUP
         GPIO.setmode(GPIO.BCM)
-        
+
         GPIO.setup(qdmpin,GPIO.OUT)
         GPIO.setup(ignitionpin,GPIO.OUT)
         GPIO.setup(rocketlogpin,GPIO.OUT)
         GPIO.output(rocketlogpin,True)
         GPIO.setup(stabilizationpin,GPIO.OUT)
-        
+
         self.balloon = None
         # self.rocket = None
 
         self.c = Comm.get_instance()
-        
+
         self.commands = queue.Queue(maxsize=10)
+
+        # Set up info logging
+        self.console = logging.getLogger('control')
+        _format = "%(asctime)s %(threadName)s %(levelname)s > %(message)s"
+        logging.basicConfig(
+            level=logging.INFO, filename='avionics/src/control.py', filemode='a', format=_format
+        )
 
     def qdm_check(self, QDM):
         '''
         This checks if we need to QDM.
         Parameter: QDM
-        
+
         if QDM = 0, QDM initiated
         else, do nothing
-        
+
         return void
         '''
-                
+
         if QDM == 1:
             GPIO.output(self.qdmpin,True)
         else:
@@ -64,7 +72,8 @@ class Control:
             data = self.generate_status_json()
             data["QDM"] = 1
             self.c.send(data, "status")
-        
+            logging.info("QDM initiated")
+
         return 0
 
     def ignition(self, mode):
@@ -73,13 +82,13 @@ class Control:
         Parameters: - mode: test mode or pre-launch mode
                     - datarange: compare data btw two computers
                     - datain: data from sensors
-        
+
         test mode: flow current for 3 sec
         pre-launch mode: flow current for 10 sec
-        
+
         return void
         '''
-        
+
         launch = self.launch_condition()
         if launch:
             if (mode == 1):
@@ -92,6 +101,7 @@ class Control:
                 # class gpiozero.OutputDevice (Outputsignal, active_high(False) ,initial_value(True), pin_factory(None))
                 GPIO.output(self.rocketlogpin,False)
                 GPIO.output(self.ignitionpin,False)
+                logging.info("rocket ignition (testing)")
             elif (mode == 2):
                 # class gpiozero.OutputDevice (Outputsignal, active_high(True) ,initial_value(False), pin_factory(None))
                 GPIO.output(self.ignitionpin,True)
@@ -102,17 +112,18 @@ class Control:
                 # class gpiozero.OutputDevice (Outputsignal, active_high(False) ,initial_value(True), pin_factory(None))
                 GPIO.output(self.rocketlogpin,False)
                 GPIO.output(self.ignitionpin,False)
+                logging.info("rocket ignition")
 
         return 0
 
     def read_data(self, balloon):
 
         '''
-        This reads the data from sensors and check whether they are within range of 5%
+        This reads the data from sensors and checks whether they are within range of 5%
         Parameters: - datain: data from sensors
-        
+
         compare condition of rocket and ballon and check if their difference has percent error less than 5 %
-        
+
         return none
         '''
         alt = balloon['alt']
@@ -122,16 +133,18 @@ class Control:
         gz = balloon['gyro']['z']
 
         self.balloon = [alt, gx, gy, gz]
+        logging.debug("data received ")
 
     def launch_condition(self):
         '''
         This check Launch condition
-        
+
         return result: launch condition true or false
         '''
-        
+
         altitude = (self.balloon[0]<=25500) & (self.balloon[0] >= 24500)
         spinrate = (math.sqrt(self.balloon[3]**2 + self.balloon[4]**2 + self.balloon[5]**2) <= 5)
+        logging.info(f"altitude: {altitude} - spinrate: {spinrate}")
 
         return (altitude & spinrate)
 
@@ -146,8 +159,7 @@ class Control:
             data = self.generate_status_json()
             data["Stabilization"] = 1
             self.c.send(data,"status")
+            logging.info("Stabilizing balloon")
 
     def receive_data(self):
         self.commands.put(json.loads(self.c.receive()))
-        if not self.commands.empty():
-            return self.commands.get()
