c8e068beb88f32769d73cd0b1e077af843496aba
mchartigan <hartigan@purdue.edu>
2019-12-04 12:02:29 -0500

    Removed old libraries and fixed a few problems

--- src/balloon.py
+++ src/balloon.py
@@ -37,8 +37,9 @@ def commProc(lproxy):
 
     print("Running comm_parse.py ...\n")
 
-    ctrl = Control(5, 6, rocketlogpin, 13, 0.05) #rocketlogpin currently undefined
-    #pin number 13 = stabilization pin
+    ctrl = Control(5, 6, 22, 13, 0.05) #rocketlogpin currently undefined
+    # pin 13 = stabilization pin
+    # pin 22 = rocket out pin
 
     mode = 1 # mode 1 = testmode / mode 2 = pre-launch mode
 
