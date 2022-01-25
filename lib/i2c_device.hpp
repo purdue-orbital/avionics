a6d77a5431bc6b4eed613afb6fec650307197ad9
Tremael Arrington <49664405+tr3m431@users.noreply.github.com>
2022-01-24 20:52:55 -0500

    created "I@CDevice" class

--- lib/i2c_device.hpp
+++ lib/i2c_device.hpp
@@ -1,1 +1,13 @@
+#pragma once
+#include <iostream>
 
+class I2CDevice {
+public:
+  // don't know type for constructor attributes
+  I2CDevice(int address, std::string name);
+private:
+  long int read();
+  std::vector<long int> read_block(int register_);
+  void write(int register_, int data);
+  
+};
