d0f4e3cdff5361c8f7d4c3efb3d5b20ee53ab6eb
jjhuang24 <90357877+jjhuang24@users.noreply.github.com>
2022-01-24 20:37:22 -0500

    Add function names

--- lib/i2c_device.cpp
+++ lib/i2c_device.cpp
@@ -1,1 +1,12 @@
+void read(self, register) {
+  // code to be executed
+}
+
+void read_block(self, register, num) {
+  // code to be executed
+}
+
+void write(self, register, data) {
+  // code to be executed
+}
 
