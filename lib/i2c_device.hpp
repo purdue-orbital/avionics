5ef51266491bdd8bd9e13aacbc95f6de4620d2fc
Tremael Arrington <49664405+tr3m431@users.noreply.github.com>
2022-01-26 18:28:16 -0500

    updated I2CDevice constructor

--- lib/i2c_device.hpp
+++ lib/i2c_device.hpp
@@ -6,6 +6,9 @@ public:
   // don't know type for constructor attributes
   I2CDevice(int address, std::string name);
 private:
+  std::string name;
+  int address;
+  
   long int read();
   std::vector<long int> read_block(int register_);
   void write(int register_, int data);
