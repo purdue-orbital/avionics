46db19fcffb1cf15c462035f1c087309c5d41086
tr3m431 <timewizard2500@gmail.com>
2022-02-28 20:58:51 -0500

    added bus openin g funtionality

--- lib/i2c_device.cpp
+++ lib/i2c_device.cpp
@@ -1,25 +1,46 @@
-#include <linux/i2c-dev.h>
-#include <i2c/smbus.h>
+// #include <linux/i2c-dev.h>
+// #include <i2c/smbus.h>
 #include "i2c_device.hpp"
 
 I2CDevice::I2CDevice(int address_, std::string name_) {
   name = name_;
   address = address_;
+  bus = open_smbus();
+  //bus = smbus(1); // open i2c bus 1
 }
-  
+
+// TODO: currently causing a linker error 
+int I2CDevice::open_smbus(){
+  int file;
+  char* filename = "/dev/i2c-1";
+  if ((file = open(filename, O_RDWR)) < 0) {
+    /* ERROR HANDLING: you can check errno to see what went wrong */
+    perror("Failed to open the i2c bus");
+    exit(1);
+  }
+
+  return file;
+}
+
 long int I2CDevice::read(int register_) {
   // code to be executed
 }
 
-long int I2CDevice::read_block(int register_, int num) {
+std::vector<long int> I2CDevice::read_block(int register_, int num) {
   // code to be executed
+  std::vector<long int> data;
+
+  for (int i=0; i<num; i++)
+    data.push_back(read(register_+i));
+
+  return data;
 }
 
 long int I2CDevice::write(int register_, int data) {
   // code to be executed
 }
 
-/*    PYTHON CODE THAT NEEDS TO BE CONVERTED TO C++ ABOVE: 
+/*    PYTHON CODE THAT NEEDS TO BE CONVERTED TO C++ ABOVE:
       def __init__(self, address, name):
         self.address = address
         self.name = name
@@ -27,16 +48,16 @@ long int I2CDevice::write(int register_, int data) {
         # Set bus to /dev/i2c-1
         self.bus = smbus.SMBus(1)
 
-    def read(self, register):
+      def read(self, register):
         return self.bus.read_byte_data(self.address, register)
 
-    def read_block(self, register, num):
+      def read_block(self, register, num):
         data = []
         for i in range(0, num):
-            data.append(self.read(register + i))
-            
+          data.append(self.read(register + i))
+
         return data
-    
-    def write(self, register, data):
+
+      def write(self, register, data):
         self.bus.write_byte_data(self.address, register, data)
 */
--- lib/i2c_device.hpp
+++ lib/i2c_device.hpp
@@ -1,16 +1,24 @@
 #pragma once
+
 #include <string>
+#include <vector>
+#include <fcntl.h>
 
 class I2CDevice {
+/*
+  parent class for all sensors
+*/
 public:
   // don't know type for constructor attributes
   I2CDevice(int address, std::string name);
 private:
   std::string name;
   int address;
+  int bus; //open working i2c bus (using bus 1 in python implementation)
 
-  long int read();
-  std::vector<long int> read_block(int register_);
-  void write(int register_, int data);
+  int open_smbus();
+  long int read(int register_);
+  std::vector<long int> read_block(int register_, int num);
+  long int write(int register_, int data);
 
 };
